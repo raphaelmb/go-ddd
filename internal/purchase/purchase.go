83bfec59ec90ddf9427778c2d3675d65db2c2998
Raphael Berger <raphaelmb@gmail.com>
2024-02-26 15:29:32 -0300

    Added purchase repository

--- internal/purchase/purchase.go
+++ internal/purchase/purchase.go
@@ -1,6 +1,8 @@
 package purchase
 
 import (
+	"context"
+	"errors"
 	"time"
 
 	"github.com/Rhymond/go-money"
@@ -15,7 +17,62 @@ type Purchase struct {
 	Store              store.Store
 	ProductsToPurchase []coffeeco.Product
 	total              money.Money
-	PaymeantMeans      payment.Means
+	PaymentMeans       payment.Means
 	timeOfPurchase     time.Time
-	CardToken          *string
+	cardToken          *string
+}
+
+func (p *Purchase) validateandEnrich() error {
+	if len(p.ProductsToPurchase) == 0 {
+		return errors.New("purchase must consist of at least one prouduct")
+	}
+
+	p.total = *money.New(0, "USD")
+
+	for _, v := range p.ProductsToPurchase {
+		newTotal, _ := p.total.Add(&v.BasePrice)
+		p.total = *newTotal
+	}
+
+	if p.total.IsZero() {
+		return errors.New("purchase should not be zero")
+	}
+
+	p.id = uuid.New()
+	p.timeOfPurchase = time.Now()
+
+	return nil
+}
+
+type CardChargeService interface {
+	ChangeCard(ctx context.Context, amount money.Money, cardToken string) error
+}
+
+type Service struct {
+	cardService  CardChargeService
+	purchaseRepo Repository
+}
+
+func (s Service) CompletePurchase(ctx context.Context, purchase *Purchase) error {
+	if err := purchase.validateandEnrich(); err != nil {
+		return err
+	}
+
+	switch purchase.PaymentMeans {
+	case payment.MEANS_CARD:
+		if err := s.cardService.ChangeCard(ctx, purchase.total, *purchase.cardToken); err != nil {
+			return errors.New("card change failed")
+		}
+	case payment.MEANS_CASH:
+	// TODO
+
+	default:
+		return errors.New("unknown payment type")
+	}
+
+	if err := s.purchaseRepo.Store(ctx, *purchase); err != nil {
+		return errors.New("failed to store purchase")
+	}
+
+	return nil
 }
